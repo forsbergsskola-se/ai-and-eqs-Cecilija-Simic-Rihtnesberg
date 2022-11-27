362d1534816d5cf61e9358de0dfce02fa401c9e0
Cecilija-Simic-Rihtnesberg <cecilija.simic.rihtnesberg@student.forsbergsskola.se>
2022-11-27 21:22:53 +0100

    Add GP_PathFollowingCopmoment

---
+++ Source/ai-and-eqs-Cecilija-Simic-Rihtnesberg/Private/GP_PathFollowingCopmoment.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GP_PathFollowingCopmoment.h"
+
+
+// Sets default values for this component's properties
+UGP_PathFollowingCopmoment::UGP_PathFollowingCopmoment()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGP_PathFollowingCopmoment::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UGP_PathFollowingCopmoment::TickComponent(float DeltaTime, ELevelTick TickType,
+                                                FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UGP_PathFollowingCopmoment::DrawSphereAtTargetDestination(FLinearColor Color, float Time)
+{
+	
+	//FNavPathSharedPtr Path = GetPath();
+	if (!Path)
+		return;
+
+	TArray<FNavPathPoint> PathPoints = Path->GetPathPoints();
+
+	
+
+	for (FNavPathPoint Point : PathPoints)
+	{
+		DrawDebugSphere(GetWorld(), Point, 60, 12, Color.ToFColorSRGB(), false, Time);
+	}
+	
+	
+}
---
+++ Source/ai-and-eqs-Cecilija-Simic-Rihtnesberg/Public/GP_PathFollowingCopmoment.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Navigation/PathFollowingComponent.h"
+#include "GP_PathFollowingCopmoment.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class AIANDAQS_API UGP_PathFollowingCopmoment : public UPathFollowingComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UGP_PathFollowingCopmoment();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable)
+	void DrawSphereAtTargetDestination(FLinearColor Color, float Time);
+};
